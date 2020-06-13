475b247e21c9180e077054415b34176115ff71ac
bystritskiy123 <gor_ell@mail.ru>
2020-06-13 21:23:28 +0300

    v1.4

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -40,10 +40,16 @@
         </div>
 
         <!--Calculator-->
-
+        <div id="Calculator">
+            <div id="gallery">
+                <label v-for="img in $options.imgNames" :key="img" style="margin-right: 2rem">
+                    <input type="radio" :value="img" v-model="selectedimg">
+                    {{img}}
+                </label>
+            </div>
+            <img :src="imgImage" :alt="selectedimg">
+        </div>
         <!--Calculator-->
-
-
     </div>
 </template>
 
@@ -53,9 +59,24 @@
         name: 'Calculator',
         data() {
             return {
-
+                selectedimg: ""
             }
-        }
+        },
+        computed: {
+            imgImage() {
+                // Return nothing for the default empty string
+                if (!this.selectedimg) {
+                    return;
+                }
+
+                const fileName = this.selectedimg.toLowerCase();
+
+                // Request the image as a webpack module by using `require`
+                return require(`../assets/${fileName}.png`);
+            }
+        },
+        // A small trick to have non-reactive elements in the component.
+        imgNames: ["img_1", "img_2", "img_3", "img_4", "img_5"]
     }
 </script>
 
@@ -79,7 +100,20 @@
             }
         }
 
+        #Calculator {
+            #gallery {
+                display: flex;
+                justify-content: space-around;
+                align-items: center;
+                width: 100%;
+                margin-left: 1%;
+            }
 
+            img {
+                margin-left: 10%;
+                margin-top: 10%;
+            }
+        }
 
     }
 
